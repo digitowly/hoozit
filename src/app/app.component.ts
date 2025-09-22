e1d27bab053fd0841a5c11417371c835f556b403
ropfoo <ropfoo@gmail.com>
2025-09-22 23:48:10 +0200

    feat: add routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { OccurrenceSearchComponent } from './features/search/components/occurrence-search/occurrence-search.component';
+import { NavigationComponent } from './features/navigation/navigation.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [FormsModule, OccurrenceSearchComponent],
+  imports: [FormsModule, NavigationComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('../app/features/map/map.component').then((m) => m.MapComponent),
+  },
+  {
+    path: 'user',
+    loadComponent: () =>
+      import('../app/features/user/user.component').then(
+        (m) => m.UserComponent
+      ),
+  },
+];
