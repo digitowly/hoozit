e167f5c672435e08648e3a088fbbb2b58256e82f
ropfoo <ropfoo@gmail.com>
2025-09-23 08:44:16 +0200

    feat: add user location service

---
+++ src/app/services/user-location/user-location.service.ts
@@ -0,0 +1,28 @@
+import { Injectable, signal } from '@angular/core';
+import { Coordinate } from '../../model/coordinate';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserLocationService {
+  coordinate = signal<Coordinate>({ latitude: 0, longitude: 0 });
+
+  getLocation() {
+    if (!navigator.geolocation) {
+      console.error('Geolocation is not supported by your browser');
+      return;
+    }
+
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        this.coordinate.set({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+        });
+      },
+      (error) => {
+        console.error('Error getting location:', error);
+      }
+    );
+  }
+}
