a71ad99bad3954fcbb154210f9689e213ca838f5
ropfoo <ropfoo@gmail.com>
2026-05-13 19:40:17 +0200

    feat(login): improve layout and support flexible login button types

--- src/app/components/login-button/login-button.component.ts
+++ src/app/components/login-button/login-button.component.ts
@@ -1,18 +1,23 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, input, output } from '@angular/core';
 import { environment } from '../../../environments/environment';
 import { IconComponent } from '../icon/icon.component';
 import { IconName } from '../../services/icon-registry/icon-registry.model';
+import { NgTemplateOutlet } from '@angular/common';
 
-export type LoginProvider = 'google' | 'apple';
+export type LoginProvider = 'google' | 'apple' | 'email';
 
-const PROVIDER_CONFIG: Record<LoginProvider, { icon: IconName; label: string }> = {
-  google: { icon: 'google', label: 'Login with Google' },
-  apple: { icon: 'apple', label: 'Login with Apple' },
+const PROVIDER_CONFIG: Record<
+  LoginProvider,
+  { icon: IconName; label: string; isLink?: boolean }
+> = {
+  google: { icon: 'google', label: 'Login with Google', isLink: true },
+  apple: { icon: 'apple', label: 'Login with Apple', isLink: true },
+  email: { icon: 'mail', label: 'Login with Email', isLink: false },
 };
 
 @Component({
   selector: 'login-button',
-  imports: [IconComponent],
+  imports: [IconComponent, NgTemplateOutlet],
   templateUrl: './login-button.component.html',
   styleUrl: './login-button.component.scss',
 })
@@ -20,5 +25,13 @@ export class LoginButtonComponent {
   readonly provider = input.required<LoginProvider>();
 
   readonly config = computed(() => PROVIDER_CONFIG[this.provider()]);
-  readonly href = computed(() => `${environment.rangoUrl}/user/login/${this.provider()}`);
+  readonly href = computed(
+    () => `${environment.rangoUrl}/user/login/${this.provider()}`,
+  );
+
+  click = output<void>();
+
+  onClick() {
+    this.click.emit();
+  }
 }
