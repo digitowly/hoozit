39cea8d2673ec5df077c29ea4cf31c69f9caf45d
ropfoo <ropfoo@gmail.com>
2026-05-19 15:14:34 +0200

    refactor(environment): update scout API URL to use new endpoint

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   verdexUrl: 'https://hoozit.app/api/verdex',
-  scoutUrl: 'https://hoozit.app/api/rango',
+  scoutUrl: 'https://hoozit.app/api/scout',
   gbifUrl: 'https://api.gbif.org/v1',
 };
