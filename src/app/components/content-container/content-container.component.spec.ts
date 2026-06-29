5612466b18d691f545cfbc08f733e23019cf6cfc
ropfoo <ropfoo@pm.me>
2026-06-29 10:20:07 +0200

    feat: update submitted occurrence styling

--- src/app/components/content-container/content-container.component.spec.ts
+++ src/app/components/content-container/content-container.component.spec.ts
@@ -1,23 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { describe, beforeEach, it, expect } from "vitest";
+import { ContentContainerComponent } from "./content-container.component";
 
-import { ContentContainerComponent } from './content-container.component';
-
-describe('ContentContainerComponent', () => {
+describe("ContentContainerComponent", () => {
   let component: ContentContainerComponent;
   let fixture: ComponentFixture<ContentContainerComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [ContentContainerComponent]
-    })
-    .compileComponents();
+      imports: [ContentContainerComponent],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(ContentContainerComponent);
     component = fixture.componentInstance;
     await fixture.whenStable();
   });
 
-  it('should create', () => {
+  it("should create", () => {
     expect(component).toBeTruthy();
   });
 });
